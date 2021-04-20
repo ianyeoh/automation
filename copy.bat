ef8d10562fde40794c30503e97d7f6d2a3cfaa97
ianyeoh <71646307+ianyeoh@users.noreply.github.com>
2021-04-20 12:17:30 +1000

    Update copy.bat
    copy.bat no longer creates zero-length log files if nothing is moved.
    
    Also updated comments for easier code-readability.

--- copy.bat
+++ copy.bat
@@ -1,16 +1,19 @@
 @echo off
 
 :: Source Directory
-SET "source=.."
+SET "source=.\"
 
 :: Destination Directory
-SET "destination=..\errlog"
+SET "destination=.\errlog"
+
+:: Temp File Directory, stores list of copied files for later use. Generated on script run and deleted on close. 
+SET "tmp_dir=.\copiedfiles_tmp.txt"
 
 :: Include These Files
 SET "include_file=*.*"
 
 :: Exclude These Files
-SET "exclude_file=autosend.* autorun_txt.*"
+SET "exclude_file=autosend.* autorun_txt.* %tmp_dir%"
 
 :: Exclude Files Newer/Older Than
 SET "exclude_if=/MINAGE:1"
@@ -18,34 +21,56 @@ SET "exclude_if=/MINAGE:1"
 
 :: Script Log File Output Directory and File Name
 SET "log_name=errlog.log"
-SET "log_directory=.\%log_name%"
+SET "log_dir=.\%log_name%"
 
 
+:: ---------------------------------------
 
-IF EXIST %log_directory% (
-GOTO :continue
-) ELSE (
-echo Errorlog for %0. Exit codes refer to robocopy exit codes. > %log_directory%
-)
-
-:continue
+:: Recording script start run time
 SET "start_date=%DATE%"
 SET "start_time=%TIME%"
 
-robocopy %source% %destination% %include_file% /XF %exclude_file% %0 %log_name% %exclude_if% /MOV /R:10 /W:5 /njh /njs /ndl /np /nc /ns /ts /xx >> %log_directory%
-
-if %ERRORLEVEL% GEQ 8 (
-	echo 			 	Started: %start_date%-%start_time% Completed: %DATE%-%TIME% Exit code: %ERRORLEVEL% >> %log_directory%
-	echo 				Error has occurred, check exit code for more information. >> %log_directory%
+:: In the case temp file exists prior to running the file, delete it (NOTE: this is unexpected behaviour, as the script deletes the temp file before closing).
+if EXIST %tmp_dir% (
+	del %tmp_dir%
 )
 
+:: Runs copy command, creates and stores a list of copied files in temp file.
+
+robocopy %source% %destination% %include_file% /XF %exclude_file% %0 %log_name% %exclude_if% /MOV /R:10 /W:5 /njh /njs /ndl /np /nc /ns /ts /xx > %tmp_dir%
+
+:: Writes to log file if robocopy returns errors.
+if %ERRORLEVEL% GEQ 8 (
+	:: Creates log file if not already present
+	if NOT EXIST %log_dir% (
+		echo. 2> %log_dir%
+	)
+	echo Started: %start_date%-%start_time% Completed: %DATE%-%TIME% Exit code: %ERRORLEVEL% >> %log_dir%
+	echo Error occurred while copying files, check robocopy exit code above for more information. >> %log_dir%
+)
+
+:: Writes to log file if files are copied. Does not generate a zero length file if no files are copied.
 SET "copied=0"
 if %ERRORLEVEL% EQU 5 SET "copied=1"
 if %ERRORLEVEL% EQU 4 SET "copied=1"
 if %ERRORLEVEL% EQU 3 SET "copied=1"
 if %ERRORLEVEL% EQU 1 SET "copied=1"
 if "%copied%" == "1" (
-	echo 			 	Started: %start_date%-%start_time% Completed: %DATE%-%TIME% Exit code: %ERRORLEVEL% >> %log_directory%
-	echo 				One or more files moved successfully. >> %log_directory%
+	:: Creates log file if not already present.
+	if NOT EXIST %log_dir% (
+		echo. 2> %log_dir%
+	)
+
+	echo Started: %start_date%-%start_time% Completed: %DATE%-%TIME% Exit code: %ERRORLEVEL% >> %log_dir%
+	for /f "tokens=*" %%a in (%tmp_dir%) do (
+  		echo %%a >> %log_dir%
+	)
+	echo One or more files moved. >> %log_dir%
 )
+
+:: Deleting temp file.
+if EXIST %tmp_dir% (
+	del %tmp_dir%
+)
+
 EXIT /B
